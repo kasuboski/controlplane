73cb274bd487d05d1a1303446e0ebfb9ab642425
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-03-29 10:53:15 -0500

    make example be about pipelines

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::{resource::{Generic, ResourceGroup, ResourceMetadata}, storage::ResourceStore};
 use resource::{Namespace, Project, Resource};
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
 use storage::MemoryStore;
 
 mod resource;
@@ -19,27 +20,102 @@ fn run() -> anyhow::Result<()> {
     store.write(&ns)?;
 
     #[derive(Deserialize, Serialize, Clone)]
-    struct MyResource {
-        pub hello: String,
+    struct Repo {
+        pub url: String,
     }
 
-    let mine = Generic {
+    #[derive(Deserialize, Serialize, Clone)]
+    struct PipelineProvider {
+        pub url: String,
+    }
+
+    #[derive(Deserialize, Serialize, Clone)]
+    struct PipelineConfig {
+        pub url: String,
+    }
+
+    #[derive(Deserialize, Serialize, Clone)]
+    struct PipelineRun {
+        pub url: String,
+        pub pass: bool,
+    } 
+
+    let pipeline_provider = Generic {
         group: ResourceGroup {
             api_version: "josh/v1".to_string(),
-            kind: "MyResource".to_string(),
+            kind: "PipelineProvider".to_string(),
         },
         metadata: ResourceMetadata {
-            name: "mine".to_string(),
+            name: "github-actions".to_string(),
             owner_ref: Some(ns.resource_ref()),
             ..Default::default()
         },
-        spec: MyResource {
-            hello: "world".to_string(),
+        spec: PipelineProvider {
+            url: "https://github.com".to_string(),
         },
     };
 
-    store.write(&mine)?;
-    let read: Generic<MyResource> = store.read(&mine.resource_ref())?;
+    store.write(&pipeline_provider)?;
+
+    let pipeline_config = Generic {
+        group: ResourceGroup {
+            api_version: "josh/v1".to_string(),
+            kind: "PipelineConfig".to_string(),
+        },
+        metadata: ResourceMetadata {
+            name: "build-image".to_string(),
+            owner_ref: Some(pipeline_provider.resource_ref()),
+            ..Default::default()
+        },
+        spec: PipelineProvider {
+            // doesn't actually exist....
+            url: "https://github.com/kasuboski/controlplane/blob/ad5b20cb7c99fa1b4c353638aaa32bb773b42d74/.github/workflows/build.yaml".to_string(),
+        },
+    };
+
+    store.write(&pipeline_config)?;
+
+    let pipeline_run = Generic {
+        group: ResourceGroup {
+            api_version: "josh/v1".to_string(),
+            kind: "PipelineRun".to_string(),
+        },
+        metadata: ResourceMetadata {
+            name: "run-1".to_string(),
+            owner_ref: Some(pipeline_config.resource_ref()),
+            labels: BTreeMap::from([
+                ("joshcorp.co/repo".to_string(), "controlplane".to_string()),
+            ]),
+            ..Default::default()
+        },
+        spec: PipelineRun {
+            url: "https://github.com/controlplane/actions/runs/1".to_string(),
+            pass: true,
+        },
+    };
+    store.write(&pipeline_run)?;
+
+    let repo = Generic {
+        group: ResourceGroup {
+            api_version: "josh/v1".to_string(),
+            kind: "Repo".to_string(),
+        },
+        metadata: ResourceMetadata {
+            name: "controlplane".to_string(),
+            owner_ref: Some(ns.resource_ref()),
+            ..Default::default()
+        },
+        spec: Repo {
+            url: "https://github.com/kasuboski/controlplane".to_string(),
+        },
+    };
+
+    store.write(&repo)?;
+    let read: Generic<Repo> = store.read(&repo.resource_ref())?;
+    println!("read: {}", serde_json::to_string_pretty(&read)?);
+
+    let read: Generic<PipelineRun> = store.read(&pipeline_run.resource_ref())?;
     println!("read: {}", serde_json::to_string_pretty(&read)?);
     Ok(())
 }
+
